e867283fb8bc6d2de3a1a69be233787f6ab96dae
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-07-20 16:43:09 -0300

    Added main profile section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainProfileComponent } from './components/main-profile/main-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainProfileComponent
   ],
   imports: [
     BrowserModule
