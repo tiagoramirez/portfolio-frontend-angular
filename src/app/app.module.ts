c482c3bf17dc26187ef08cd3bdec21306a301b54
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-07-21 17:09:03 -0300

    Added about me section and minimized the width of the entire project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainProfileComponent } from './components/main-profile/main-profile.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainProfileComponent
+    MainProfileComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
