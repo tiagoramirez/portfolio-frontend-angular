c7b639cacdc71f23bb9ea455e811e8d4c8efd9ad
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-07-21 22:12:18 -0300

    Added experience section but without style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainProfileComponent } from './components/main-profile/main-profile.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceEducationComponent } from './components/experience-education/experience-education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainProfileComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ExperienceEducationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/experience-education/experience-education.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-education',
+  templateUrl: './experience-education.component.html',
+  styleUrls: ['./experience-education.component.css']
+})
+export class ExperienceEducationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
