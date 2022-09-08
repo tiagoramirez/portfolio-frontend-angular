75c5d6e081fdbcead7cfc3484921e578c2e761fc
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-09-08 17:31:23 -0300

    Finished catching errors in delete social media section. Other section related with social media remaining

--- src/app/modules/social-media/components/delete-social-media/delete-social-media.component.ts
+++ src/app/modules/social-media/components/delete-social-media/delete-social-media.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppSettings } from 'src/app/helpers/appSettings';
+import { SubscriptionContainer } from 'src/app/helpers/subscriptionContainer';
+import { SocialMediaService } from 'src/app/services/social-media.service';
 
 @Component({
-  selector: 'app-delete-social-media',
-  templateUrl: './delete-social-media.component.html',
-  styleUrls: ['./delete-social-media.component.css']
+    selector: 'app-delete-social-media',
+    templateUrl: './delete-social-media.component.html',
+    styleUrls: ['./delete-social-media.component.css']
 })
 export class DeleteSocialMediaComponent implements OnInit {
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private socialMediaService: SocialMediaService, private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.username = this.route.snapshot.params['username'];
+        this.socialMediaId = this.route.snapshot.params['idSm'];
+    }
 
+    ngOnDestroy(): void {
+        this.subsContainer.unsubscribeAll();
+    }
+
+    delete() {
+        this.isErrorLoadingNewData = false;
+        this.loadingNewData = true;
+        let sub = this.socialMediaService.delete(this.socialMediaId).subscribe({
+            error: (err) => {
+                if (err.error.messageControlled !== undefined && err.error.messageControlled == true) {
+                    this.errorMessageLoadingNewData = err.error.message;
+                }
+                else {
+                    this.errorMessageLoadingNewData = AppSettings.serverErrorMessage;
+                }
+                this.isErrorLoadingNewData = true;
+                this.loadingNewData = false;
+            },
+            complete: () => {
+                this.loadingNewData = false;
+                this.subsContainer.add(sub);
+                this.router.navigate(['/' + this.username + '/social-media/list']);
+            }
+        });
+    }
+
+    username: string;
+    socialMediaId: number;
+
+    subsContainer: SubscriptionContainer = new SubscriptionContainer();
+
+    loadingNewData: boolean = false;
+    errorMessageLoadingNewData: string = '';
+    isErrorLoadingNewData: boolean = false;
 }
